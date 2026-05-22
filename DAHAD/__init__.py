0317f4f46685da8bc0056808e09c29377624b5a0
favila <felipeavila41@gmail.com>
2026-05-22 19:45:18 -0400

    few edits

--- DAHAD/__init__.py
+++ DAHAD/__init__.py
@@ -1,14 +1,14 @@
-import matplotlib.pyplot as plt 
-
-
-plt.rcParams.update({
-    "font.size": 18,          # base font size
-    "axes.titlesize": 22,     # title
-    "axes.labelsize": 20,     # x and y labels
-    "xtick.labelsize": 18,    # x tick labels
-    "ytick.labelsize": 18,    # y tick labels
-    "legend.fontsize": 18,    # legend text
-    "legend.title_fontsize": 18,
-    "figure.titlesize": 24,   # figure title
-    "figure.figsize": (14, 10),
-})
+# import matplotlib.pyplot as plt
+#
+#
+# plt.rcParams.update({
+#     "font.size": 18,          # base font size
+#     "axes.titlesize": 22,     # title
+#     "axes.labelsize": 20,     # x and y labels
+#     "xtick.labelsize": 18,    # x tick labels
+#     "ytick.labelsize": 18,    # y tick labels
+#     "legend.fontsize": 18,    # legend text
+#     "legend.title_fontsize": 18,
+#     "figure.titlesize": 24,   # figure title
+#     "figure.figsize": (14, 10),
+# })
